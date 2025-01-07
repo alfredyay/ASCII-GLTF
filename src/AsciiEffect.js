6b977eb5793ec31980ad76c0334c55373ba98c4b
alfredyay <53280045+alfredyay@users.noreply.github.com>
2025-01-07 12:08:58 -0500

    Update AsciiEffect.js

--- src/AsciiEffect.js
+++ src/AsciiEffect.js
@@ -10,7 +10,7 @@ class AsciiEffect {
         const bBlock = options['block'] || false; // Blocky character style
         const bInvert = options['invert'] || false; // Invert colors
         const strResolution = options['strResolution'] || 'low';
-        const fontFamily = options['fontFamily'] || 'Consolas, monospace'; // Custom font family
+        const fontFamily = options['fontFamily'] || '"Arial", sans-serif'; // Custom font family
 
         let width, height;
 
